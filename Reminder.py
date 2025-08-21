2785e4ddada97a1e292bc768d1d725bb1a14c5f9
Inna-A-R <inna1990sweet@mail.ru>
2025-08-22 02:13:12 +0300

    Добавлена функция установки времени напоминания.

--- Reminder.py
+++ Reminder.py
@@ -5,12 +5,28 @@ import datetime
 import time
 import pygame
 
- window = Tk()
- window.title('Напоминание')
- label = Label(text='Установите напоминание')
- label.pack(pady=10)
- set_button = Button(text='Установить напоминание', command=set)
- set_button.pack()
 
- window.mainloop()
+def set():
+    rem = sd.askstring(title='Время напоминания', prompt='Введите время напоминания в формате ЧЧ:ММ ( в 24 часовом формате)')
+    if rem:
+        try:
+            hour = int(rem.split(':')[0])
+            minute = int(rem.split(':')[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror(title='Ошибка', message=f'Произошла ошибка {e}')
 
+
+window = Tk()
+window.title('Напоминание')
+label = Label(text='Установите напоминание')
+label.pack(pady=10)
+set_button = Button(text='Установить напоминание', command=set)
+set_button.pack()
+
+window.mainloop()
